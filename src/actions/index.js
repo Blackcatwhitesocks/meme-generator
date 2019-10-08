bacdca8f7b26b3b6ecb466356b1a1d95754c986a
Katya Neumann <katya.neumann@me.com>
2019-10-07 19:43:23 -0700

    created meme items

---
+++ src/actions/index.js
@@ -0,0 +1,24 @@
+import { CoverageSummary } from "istanbul-lib-coverage";
+
+export const RECEIVE_MEMES = 'RECEIVE_MEMES';
+
+function receiveMemes(json) {
+    const { memes } = json.data;
+    
+    return {
+        type: RECEIVE_MEMES,
+        memes
+    }
+}
+
+function fetchMemesJson() {
+    return fetch('https://api.imgflip.com/get_memes')
+        .then(response => response.json())
+}
+
+export function fetchMemes() {
+    return function(dispatch) {
+        return fetchMemesJson()
+        .then(json  => dispatch(receiveMemes(json)))
+    }
+}
---
+++ src/actions/secrets.js
@@ -0,0 +1,5 @@
+const username = 'katyanemann';
+const passwrod = 'qoqgiv-dEcqac-2topra';
+
+export { username, password };
+
